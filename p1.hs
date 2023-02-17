c43a51bf1027122d460f07fd1f38b5c954089390
esept <64868182+esept@users.noreply.github.com>
2023-02-17 20:08:33 +0100

    Add files via upload
    challengeN a faire

--- p1.hs
+++ p1.hs
@@ -1,16 +1,5 @@
--- data Formula = Var String
---     | T 
---     | F 
---     | Not Formula
---     | And Formula Formula
---     | Or Formula Formula
---     | Imp Formula Formula
---     | Eqv Formula Formula
---     deriving (Show,Eq,Read)
-
--- type World = [String]
-
 import CPL
+------------------------------------------------------------------------------
 
 door1 :: Formula
 door1 = And (Var "p1") (Var "t2")
@@ -26,7 +15,7 @@ reglement = Or (And (Not door1) door2) (And door1 (Not door2))
 
 challenge1 :: Formula
 challenge1 = And reglement constraint
-
+------------------------------------------------------------------------------
 
 w0 :: World
 w0 = ["p1","p2"]
@@ -36,21 +25,62 @@ w1 = ["t1","t2"]
 
 w2 :: World
 w2 = ["p1","p2","t1","t2"]
+------------------------------------------------------------------------------
 
+genOne :: [String] -> [World]
 genOne [] = []
 genOne (w:ws) = [w] : genOne ws
 
+ajoutAll :: World -> [World] -> [World]
 ajoutAll _ [] = []
-ajoutAll a (b:bs) = (a++b) :(ajoutAll a bs)
+ajoutAll a (b:bs) = ((a++b) :(ajoutAll a bs))
 
+genAll :: [World] -> [World]
 genAll (a:[]) = []
 genAll (w:ws) = (genAll ws ) ++ (ajoutAll w ws) ++ (ajoutAll w (genAll ws))
+
+genAllWorlds:: [String] -> [World]
 genAllWorlds w = (genOne w ) ++ genAll (genOne w) ++ [[]]
 
 
 test1 = ["a","b","c","d"]
 test2 = [["a"],["b"],["c"],["d"]]
 test3 = [["e","a"],["e","b"],["e","c"],["e","d"]]
+------------------------------------------------------------------------------
 
+countWorld :: [World] -> Int
 countWorld [] = 0
 countWorld (w:ws) = 1 + countWorld ws
+------------------------------------------------------------------------------
+
+
+sat :: World -> Formula -> Bool
+sat _ T = True
+sat _ F = False
+sat w (Not f) = not(sat w f)
+sat [] _ = False
+sat (w:ws) (Var f)
+    | w == f = True
+    | otherwise = sat ws (Var f)
+sat ws (And f1 f2) = (sat ws f1) && (sat ws f2)
+sat ws (Or f1 f2) = (sat ws f1) || (sat ws f2)
+sat ws (Imp f1 f2)
+    | (sat ws f1) == True && (sat ws f2) == False = False
+    | otherwise = True
+sat ws (Eqv f1 f2) = (sat ws f1) == (sat ws f2)
+
+------------------------------------------------------------------------------
+findWorlds :: Formula -> [World]
+findWorlds w = genAllWorlds(extrait w )
+
+extrait T = []
+extrait F = []
+extrait (Var w) = w : []
+extrait (Not f) = extrait f
+extrait (And f1 f2) = (extrait f1) ++ (extrait f2) 
+extrait (Imp f1 f2) = (extrait f1) ++ (extrait f2) 
+extrait (Or f1 f2) = (extrait f1) ++ (extrait f2) 
+extrait (Eqv f1 f2) = (extrait f1) ++ (extrait f2) 
+
+------------------------------------------------------------------------------
+challengeN :: Formula

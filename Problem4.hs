c52c5bb6659a60222e28655f45465137affa6489
esept <64868182+esept@users.noreply.github.com>
2023-03-18 21:24:30 +0100

    Add files via upload

--- Problem4.hs
+++ Problem4.hs
@@ -13,33 +13,39 @@ interp "b2" = [12,32]
 interp "b3" = [23,43]
 interp "b4" = [34]
 
+interp _ = []
 
 indis "a" 01 = [01]
 indis "a" 10 = [10,12]
-
 indis "a" 12 = indis "a" 10
 indis "a" 21 = [12,23]
-
 indis "a" 23 = [12,23]
 indis "a" 32 = indis "a" 23
-
 indis "a" 34 = [34,23]
 indis "a" 43 = indis "a" 34
 
 
-indis "b" 10 = [01]
+indis "b" 10 = [10]
 indis "b" 01 = [01,12]
 indis "b" 21 = [01,21]
+indis "b" 12 = [12,32]
+indis "b" 23 = [23,43]
+indis "b" 32 = [32,12]
+indis "b" 43 = [23,43]
+indis "b" 34 = [34]
 
-
-
-
+s0 :: EpiState
+s0 = (interp , indis ,12)
 
 anneIgn :: EpiFormula 
-anneIgn = Not (Knows "a" (Var "bs"))
+anneIgn = Not (Knows "a" (Var "b"))
 
-billLinorance :: EpiFormula
-billLinorance = Not (Knows "b" (Var "as"))
+billIgnorance :: EpiFormula
+billIgnorance = Not (Knows "b" (Var "a"))
 
 problem4 :: EpiFormula
-problem4 = After (And (anneIgn) (billLinorance)) (Not (anneIgn))
+problem4 = After(
+        After
+        (And (anneIgn) (billIgnorance)) 
+        (Not (anneIgn))
+    )(Not (billIgnorance))

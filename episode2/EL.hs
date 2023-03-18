ba7b841adbd5129fd41e8f118b14d3e26411403e
P-kne <66096689+P-kne@users.noreply.github.com>
2023-03-18 20:29:32 +0100

    Add files via upload

--- episode2/EL.hs
+++ episode2/EL.hs
@@ -4,6 +4,8 @@ module EL
     World,
     EpiState,
     EpiFormula (..),
+    epiSat,
+    update,
   )
 where
 
@@ -27,3 +29,30 @@ data EpiFormula
   | Knows Agent EpiFormula
   | After (EpiFormula) (EpiFormula)
   deriving (Show, Eq)
+
+contient :: Int -> [Int] -> Bool
+contient w (x : xs)
+  | w == x = True
+  | otherwise = contient w xs
+
+test :: [Bool] -> Bool
+test [] = True
+test (x : xs) = x && test xs
+
+update :: EpiState -> EpiFormula -> EpiState
+update (int, acc, w) phi = (updatedInt, updatedAcc, w)
+  where
+    updatedInt p = filter (\x -> epiSat (int, acc, x) phi) (int p)
+    updatedAcc i w' = filter (\x -> epiSat (int, acc, x) phi) (acc i w')
+
+epiSat :: EpiState -> EpiFormula -> Bool
+epiSat _ T = True
+epiSat _ F = False
+epiSat (interp, _, w) (Var p) = contient w (interp p)
+epiSat s (And phi psi) = (epiSat s phi) && (epiSat s psi)
+epiSat s (Or phi psi) = (epiSat s phi) || (epiSat s psi)
+epiSat s (Not phi) = not (epiSat s phi)
+epiSat s (Imp phi psi) = not (epiSat s phi) || (epiSat s psi)
+epiSat s (Eqv phi psi) = (epiSat s phi) == (epiSat s psi)
+epiSat (interp, indis, w) (Knows a phi) = test (map (\wi -> epiSat (interp, indis, wi) phi) (indis a w))
+epiSat (int, acc, w) (After phi psi) = (epiSat (int, acc, w) phi) && (epiSat (update (int, acc, w) phi) psi)
